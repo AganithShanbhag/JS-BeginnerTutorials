d00c0c1fa2eab219d8adebd519ff12ba5a6a1f92
Aganith Shanbhag <aganith.shanbhag@learner.manipal.edu>
2021-04-30 23:25:10 +0530

    onmouseover and onmouseout

---
+++ imageEvents.js
@@ -0,0 +1,7 @@
+function changeImg() {
+    document.getElementById("img1").src = "https://images.pexels.com/photos/4471315/pexels-photo-4471315.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+}
+
+function resetImg() {
+    document.getElementById("img1").src = "https://images.pexels.com/photos/1236678/pexels-photo-1236678.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+}
